b16e24ab3d1fa5b77ac90719655d3fa657f51f05
Mercator <tedusar@tugraz.at>
2025-06-19 16:57:11 +0200

    adjust files

--- launch/semantic_segmentation.launch.py
+++ launch/semantic_segmentation.launch.py
@@ -10,6 +10,6 @@ def generate_launch_description():
             name='semantic_segmentation_node',
             output='screen',
             #parameters=['src/semantic_segmentation/config/mercator_params.yaml']
-            parameters=['src/semantic_segmentation/config/warthog_params.yaml']
+            parameters=['/home/tedusar/aart25_mercator_ws/src/navigation/perception/segmentation/semantic_segmentation/config/mercator_params.yaml']
         )
     ])
